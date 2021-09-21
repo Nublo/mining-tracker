6a308f98fbf8162de31c027d75af90a46b1a42db
Anatol <anatol@anatols-mbp.home>
2021-09-21 21:37:32 +0100

    Mining tracker group project created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <Button variant="primary" size="sm">
+        Small button
+      </Button>
+    </div>
+  );
+}
